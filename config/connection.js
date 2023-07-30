2c5f1b502ac7326dbd21d9a295c3f092e41604ec
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-30 14:29:00 -0500

    placed gitignore and updated connection to help create the link to heroku for the use of the local host database

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const url = require('url');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
+let databaseUrl = process.env.CLEARDB_DATABASE_URL || process.env.JAWSDB_URL;
+let sequelize;
+
+if (databaseUrl) {
+  const dbUrlParts = url.parse(databaseUrl);
+  const [username, password] = dbUrlParts.auth.split(':');
+
+  sequelize = new Sequelize(dbUrlParts.pathname.substring(1), username, password, {
+    host: dbUrlParts.hostname,
     dialect: 'mysql',
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    }
+  );
+}
 
 module.exports = sequelize;
