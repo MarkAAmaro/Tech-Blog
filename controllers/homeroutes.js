8173e3cb8a6157fd6d0e4ffc2cd21587004cfccf
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-30 16:35:08 -0500

    setting console logs to show in horoku route issues

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -7,16 +7,52 @@ router.get('/', async (req, res) => {
       include: [{ model: User }],
     });
 
+    
+    console.log('Raw Blog Data:', blogData);
+
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    
+    console.log('Plain Blog Data:', blogs);
 
     res.render('home', { blogs, loggedIn: req.session.loggedIn });
   } catch (err) {
+    console.log('Error in "/" route:', err);
     res.status(500).json(err);
   }
 });
 
 router.get('/blog/:id', async (req, res) => {
-  
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+
+    
+    console.log('Raw Blog Data:', blogData);
+
+    if (!blogData) {
+      console.log('No blog found with this id:', req.params.id);
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    const blog = blogData.get({ plain: true });
+
+    
+    console.log('Plain Blog Data:', blog);
+
+    res.render('blog', { 
+      blog, 
+      loggedIn: req.session.loggedIn 
+    });
+  } catch (err) {
+    console.log('Error in "/blog/:id" route:', err);
+    res.status(500).json(err);
+  }
 });
 
-module.exports = router;
+
+
+
+
