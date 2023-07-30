7b4a72f48a64ab97c1d4dc475d04c44bd6c8946b
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-30 17:43:09 -0500

    set local host rendering using html

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./apiroutes');
+console.log('apiRoutes:', apiRoutes);
+
 const dashboardRoutes = require('./dashboardroutes');
+console.log('dashboardRoutes:', dashboardRoutes);
+
 const homeRoutes = require('./homeroutes');
+console.log('homeRoutes:', homeRoutes);
+
 const userRoutes = require('./userroutes');
+console.log('userRoutes:', userRoutes);
 
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
